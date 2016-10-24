f8e5912a85302607d75e525e18dd8c0d8f379b2e
Dhaval <dhavalhparikh26@gmail.com>
2016-10-23 21:31:29 -0400

    Django first commit

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/dhavalparikh/Documents/Dhaval_personal/djangogirls/myenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
